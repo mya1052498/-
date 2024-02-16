2fcc5a442706af1274351d35b1ec59bf754e00ce
mya1052498 <mya1052498@gmail.com>
2024-02-17 03:00:13 +0800

    無法讓物件消失

--- 1.js
+++ 1.js
@@ -1,6 +1,19 @@
-function startGame() {
-    var startButton = document.getElementById("gameStrat");
-    // var overlay = document.getElementById("overlay");
-    startButton.style.opacity = "0";
-}
-  
+// // 確保DOM已經加載完畢
+// document.addEventListener("DOMContentLoaded", function() {
+//     // 獲取按鈕元素
+//     var gameStartButton = document.getElementById('gameStart');
+//     // 獲取容器元素
+//     var homepageContainer = document.getElementById('homepageContainer');
+
+//     // 綁定點擊事件
+//     gameStartButton.onclick = gameStart;
+//     // 定義函數
+//     function gameStart() {
+//         homepageContainer.style.opacity = "0";
+//     }
+// });
+
+$('#gameStart').onclick(function() {
+    $("#homepageContainer").addClass('hidn');
+})
+
